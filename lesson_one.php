cd89c78e24cb1ce55529f82139d525a9d6b6c290
samf2022 <samf0794@gmail.com>
2024-09-06 11:32:25 -0400

    First lesson

---
+++ lesson_one.php
@@ -0,0 +1,35 @@
+<?php
+
+class Person {
+    public $firstName;
+    public $lastName;
+
+    public function __construct($firstName, $lastName){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function getFullName() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
+
+$person_one = new Person('Santiago', 'Medina');
+$person_two = new Person('Raul', 'Palacios');
+
+
+//Old way
+// $firstName = 'Santiago';
+// $lastName = 'Medina';
+
+// $firstNameTwo = 'Raul';
+// $lastNameTwo = 'Palacios';
+
+// $fullName = "$firstName $lastName";
+// $fullNameTwo = "$firstNameTwo $lastNameTwo";
+
+// echo "$fullName is friend with $fullNameTwo";
+
+echo "{$person_one->getFullName()}  is friend with {$person_two->getFullName()}";
+
+?>
