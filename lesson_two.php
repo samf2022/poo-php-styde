1a58680ca97975bda4a3031114d6ce98107e6b73
samf2022 <samf0794@gmail.com>
2024-09-09 06:26:40 -0400

    lesson two

--- lesson_two.php
+++ lesson_two.php
@@ -3,10 +3,24 @@
 class Person {
     protected $firstName;
     protected $lastName;
+    protected $nickName;
+    protected $changedNickName = 0;
 
     public function __construct($firstName, $lastName){
         $this->firstName = $firstName;
         $this->lastName = $lastName;
+    }
+
+    public function setNickname($nickName){
+        if($this->changedNickName >= 2){
+            throw new Exception("You've changed the nickname too many times");
+        }
+        $this->nickName = $nickName;
+        $this->changedNickName++;
+    }
+
+    public function getNickName(){
+        return $this->nickName;
     }
 
     public function getFullName() {
@@ -31,5 +45,13 @@ $person_two = new Person('Raul', 'Palacios');
 // echo "$fullName is friend with $fullNameTwo";
 
 echo "{$person_one->getFullName()}  is friend with {$person_two->getFullName()}";
+
+$person_one->setNickname('carlos');
+$person_one->setNickname('roy');
+$person_one->setNickname('leroy');
+$person_one->setNickname('lee');
+echo $person_one->getNickName();
+
+
 
 ?>
